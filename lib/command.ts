d77abd4a6479cdff060899344c69f5a75c6b58f2
Andrew Clunis <andrew@orospakr.ca>
2015-09-20 15:13:26 -0400

    Continuing to port to TS

---
+++ lib/command.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import util = require("util");
+
+class Command implements Operation {
+  summary: string = "UNSET!"
+	
+  execute() {
+		
+  }
+}
---
+++ lib/operation.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+// Operation base class.
+var util = require("util");
+
+interface Operation {
+    execute();
+}
